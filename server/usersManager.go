41402ae47c9fd2bf9ac68eeeb9ca494c3bc25c23
r é m i  <remi.robert@epitech.eu>
2017-12-05 14:55:37 +0800

    add broadcast in the users manager

--- server/usersManager.go
+++ server/usersManager.go
@@ -1,6 +1,10 @@
 package main
 
-import "sync"
+import (
+	"sync"
+	"log"
+	pb "grpc-go-chat/chat"
+)
 
 type UsersManager struct {
 	users map[string]User
@@ -8,18 +12,21 @@ type UsersManager struct {
 }
 
 func (u *UsersManager) Remove(id string) {
+	log.Print("[USER manager] remove new user: ", id)
 	u.mutex.Lock()
 	delete(u.users, id)
 	u.mutex.Unlock()
 }
 
 func (u *UsersManager) Add(user User) {
+	log.Print("[USER manager] add new user: ", user)
 	u.mutex.Lock()
 	u.users[user.id] = user
 	u.mutex.Unlock()
 }
 
 func (u* UsersManager) Find(id string) *User {
+	log.Print("[USER manager] find user: ", id)
 	u.mutex.Lock()
 	defer u.mutex.Unlock()
 	for _, user := range u.users {
@@ -30,7 +37,16 @@ func (u* UsersManager) Find(id string) *User {
 	return nil
 }
 
-func NewUsers() *UsersManager {
+func (u *UsersManager) BroadcastMessage(message *pb.ChatMessage) {
+	log.Print("[User manager] broadcast : ", *message)
+	u.mutex.Lock()
+	for _, user := range u.users {
+		user.stream.Send(message)
+	}
+	u.mutex.Lock()
+}
+
+func NewUsersManager() *UsersManager {
 	return &UsersManager{
 		users: make(map[string]User),
 		mutex: &sync.Mutex{},
